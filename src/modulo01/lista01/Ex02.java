ccffe6d23eccb474dbd36dd2748698cb4280dd4d
VictorDib_IOS-TotvsPC <dib_victor@outlook.com>
2023-03-01 16:27:13 -0300

    Atualização.

--- src/modulo01/lista01/Ex02.java
+++ src/modulo01/lista01/Ex02.java
@@ -1,5 +1,22 @@
 package modulo01.lista01;
+/*
+2. Calcular a quantidade de pessoas que cabem em uma sala de cinema
 
+Crie um programa que receba a quantidade de cadeiras que existe em cada fileira 
+de uma sala de cinema, e quantas fileiras existem na sala. O programa deve então imprimir 
+no console a capacidade de pessoas sentadas nessa sala.
+
+A sala de cinema tem 10 fileiras e 8 cadeiras em cada.
+A capacidade dessa sala é de 80 pessoas.
+
+Desafio extra: Considere que a sala de cinema reserve 10% de sua capacidade para pessoas 
+com deficiência. Imprima no console a quantidade total de lugares e a quantidade reservada 
+para pessoas com deficiência.
+
+A sala de cinema tem 10 fileiras e 8 cadeiras em cada.
+A capacidade dessa sala é de 80 pessoas, sendo que 8 lugares são reservados para PCDs.
+
+ */
 import java.util.Scanner;
 
 public class Ex02 {
